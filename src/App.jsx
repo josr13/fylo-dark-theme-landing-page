d07fd2108337d29c65eb163c3b746a3ab3156361
josr13 <joseph-l-r-r@hotmail.com>
2023-04-15 19:45:08 -0500

    Rename Header comp. to Navbar + add Navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <>
-      <Header />
+      <Navbar />
       <Hero />
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="" className="logoLink">
+        <img className="logo" src="src/assets/logo.svg" alt="Fylo logo" />
+      </a>
+      <ul className="navbar">
+        <li>
+          <a href="">Features</a>
+        </li>
+        <li>
+          <a href="">Team</a>
+        </li>
+        <li>
+          <a href="">Sign In</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
