3896a590a282421a2b4f9fd4810a9b514fed4310
josr13 <joseph-l-r-r@hotmail.com>
2023-04-15 22:06:12 -0500

    Add styling to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-function Hero() {
+export default function Hero() {
   return (
-    <>
+    <main>
       <img
         className="heroImg"
         src="src/assets/illustration-intro.png"
         alt="Hero image"
       />
-      <h1>All your files in one secure location, accessible anywhere.</h1>
-      <p>
-        Fylo stores all your most important files in one secure location. Access
-        them wherever you need, share and collaborate with friends family, and
-        co-workers.
-      </p>
-      <button>Get Started</button>
-    </>
+      <div className="heroDesc">
+        <h1>All your files in one secure location, accessible anywhere.</h1>
+        <p>
+          Fylo stores all your most important files in one secure location.
+          Access them wherever you need, share and collaborate with friends
+          family, and co-workers.
+        </p>
+        <button className="cta">Get Started</button>
+      </div>
+    </main>
   );
 }
-
-export default Hero;
